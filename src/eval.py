b6c3fa99ac287aafb6521fc8227cecbb84f6e2a6
Garois <djgarice@gmail.com>
2025-11-25 16:29:24 +0700

    delete previous project version

--- src/eval.py
+++
@@ -1,8 +0,0 @@
-def dice_score(pred, gt):
-    pred = pred.astype(bool)
-    gt = gt.astype(bool)
-    inter = (pred & gt).sum()
-    denom = pred.sum() + gt.sum()
-    if denom == 0:
-        return 1.0
-    return 2.0 * inter / denom
